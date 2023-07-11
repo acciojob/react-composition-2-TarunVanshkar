c4c60016a6f2e53acd0aae487a7e7f1c712dff96
TarunVanshkar <tarunben33@gmail.com>
2023-07-11 21:37:27 +0530

    submitting code-Tue Jul 11 2023 21:37:21 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
+import Modal from "./Modal";
 
 const App = () => {
+  const [visible, setVisible] = useState(false);
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <button onClick={() => setVisible(true)}>Show Modal</button>
+        {
+          visible && <Modal show={setVisible} />
+        }
     </div>
   )
 }
---
+++ src/components/Modal.js
@@ -0,0 +1,18 @@
+
+import React, { useState } from "react";
+import './../styles/App.css';
+
+const Modal = (props) => {
+
+  return (
+    <div>
+        {/* Do not remove the main div */}
+        <div className="model-overlay" onClick={() => props.show(false)}>
+            <button className="model-close" onClick={() => props.show(false)}>Close</button>
+            <p>This is the content of the modal.</p>
+        </div>
+    </div>
+  )
+}
+
+export default Modal;
